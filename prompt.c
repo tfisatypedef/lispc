6bc87f60206379922fc461983f85a3666e7c1637
tfistypedef <topkratomtx@protonmail.com>
2022-01-14 16:33:13 -0600

    ch3

--- prompt.c
+++ prompt.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <histedit.h>
 
 /* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/* static char input[2048]; */
 
 int main(int argc, char** argv) {
 
@@ -12,14 +16,24 @@ int main(int argc, char** argv) {
    /* In a never ending loop */
   while (1) {
 
+    /* Output our prompt and get input */
+    char* input = readline("lispy> ");
+
+    /* Add input to history */
+    add_history(input);
+
+    
     /* Output our prompt */
-    fputs("lispy> ", stdout);
+    /*    fputs("lispy> ", stdout); */
 
     /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* fgets(input, 2048, stdin); */
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
 
   }
 
