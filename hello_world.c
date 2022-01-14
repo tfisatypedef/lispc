78a5a5fc0684bb45252440260fd21e628701a7d4
tfistypedef <topkratomtx@protonmail.com>
2022-01-13 20:17:57 -0600

    inital commit

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  puts("Hello, world!");
+  /*
+  int i = 5;
+  while (i > 1){
+
+    i= i - 1; 
+
+      return 0;*/
+}
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+int add_together(int x, int y) {
+  int result = x + y;
+  int added = add_together(10,18);
+  return result;
+}
+}
